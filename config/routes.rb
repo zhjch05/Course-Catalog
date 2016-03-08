9e7a2b373ac844b781038bcf050cf5ddc157f562
zhjch05 <zjc1996@brandeis.edu>
2016-03-08 14:54:22 -0500

    devise 1 setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations',
+    passwords: 'users/passwords'
+  }
   get 'home/index'
   root to: 'home#index'
 
